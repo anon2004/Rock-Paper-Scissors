954634dc0da0162efbee7d9b533ad8e987c00c76
Beamer Devs <83333147+anon2004@users.noreply.github.com>
2023-11-24 23:33:46 +0300

    Added a score tracker and a better loop using break and continue

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-
-player_1=input("What is Your Name?:")
-player_2=input("What is Your Opponents Name?:")
-print("\033[34m*********************************************\033[0m")
+print("\033[34m*****************************************************\033[0m")
 print("\033[33mWELCOME TO THE EPIC ROCK PAPER SCISSORS GAME\033[0m")
 print("\033[31m***RULES***\033[0m")
 print("Enter R for Rock, P for Paper, S for Scissors")
-print("\033[34m*********************************************\033[0m")
+print("\033[34m**********************************************************\033[0m")
+player_1=input("What is Your Name?:")
+player_2=input("What is Your Opponents Name?:")
+
+player_1_score=0
+player_2_score=0
 
 
 #Here We Import Getpass To Hide THe Input
 from getpass import getpass as input
-play_1=input(f"{player_1} Enter Your Choice: ")
-play_2=input(f"{player_2} Enter Your Choice: ")
-if play_1.lower()=="r" and play_2.lower()=="s":
+
+
+while True:
+  play_1=input(f"{player_1} Enter Your Choice: ")
+  play_2=input(f"{player_2} Enter Your Choice: ")
+  if play_1.lower()=="r" and play_2.lower()=="s":
    print(f"{player_1} WINS")
-elif play_1.lower()=="s" and play_2.lower()=="r":
+   player_1_score=player_1_score+1
+  
+  elif play_1.lower()=="s" and play_2.lower()=="r":
    print(f"{player_2} WINS")
-elif play_1.lower()=="s" and play_2.lower()=="p":
+   player_2_score=player_2_score+1
+  
+  elif play_1.lower()=="s" and play_2.lower()=="p":
    print(f"{player_1} WINS")
-elif play_1.lower()=="p" and play_2.lower()=="s":
+   player_1_score=player_1_score+1
+  
+  elif play_1.lower()=="p" and play_2.lower()=="s":
    print(f"{player_2} WINS")
-elif play_1.lower()=="r" and play_2.lower()=="p":
+   player_2_score=player_2_score+1
+  
+  elif play_1.lower()=="r" and play_2.lower()=="p":
    print(f"{player_1} WINS")
-elif play_1.lower()=="p" and play_2.lower()=="r":
+   player_1_score=player_1_score+1
+  
+  elif play_1.lower()=="p" and play_2.lower()=="r":
    print(f"{player_2} WINS")
-elif play_1.lower()==play_2.lower():
+   player_2_score=player_2_score+1
+  
+  elif play_1.lower()==play_2.lower():
    print("Its a Tie")
-else:
+   player_1_score=player_1_score
+   player_2_score=player_2_score
+  else:
    print('\033[31mSOMEBODY ENTERED AN INVALID CHOICE')
-condition=input("Do You Want To Play Again? (yes/no/y/n): ")
-if condition.lower()=="yes" or condition.lower=="y":
-   break
-print("THANK YOU FOR PLAYING")
+  if player_1_score==3 or player_2_score==3:
+    break
+  else:
+    continue
+if player_1_score>player_2_score:
+  print(f"{player_1} WINS THE GAME {player_1_score}-{player_2_score}")
+else:
+  print(f"{player_2} WINS THE GAME {player_1_score}-{player_2_score}")
 
