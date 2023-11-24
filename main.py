f740c60583c2449bddd04d4529bf1f96812a073f
Beamer Devs <83333147+anon2004@users.noreply.github.com>
2023-11-24 13:56:30 +0300

    Switched from a while to a while True loop

--- main.py
+++ main.py
@@ -8,30 +8,28 @@ print("Enter R for Rock, P for Paper, S for Scissors")
 print("\033[34m*********************************************\033[0m")
 
 
-exit_condition="no"
-while exit_condition=="no":
 #Here We Import Getpass To Hide THe Input
- from getpass import getpass as input
- play_1=input(f"{player_1} Enter Your Choice: ")
- play_2=input(f"{player_2} Enter Your Choice: ")
- if play_1.lower()=="r" and play_2.lower()=="s":
+from getpass import getpass as input
+play_1=input(f"{player_1} Enter Your Choice: ")
+play_2=input(f"{player_2} Enter Your Choice: ")
+if play_1.lower()=="r" and play_2.lower()=="s":
    print(f"{player_1} WINS")
- elif play_1.lower()=="s" and play_2.lower()=="r":
+elif play_1.lower()=="s" and play_2.lower()=="r":
    print(f"{player_2} WINS")
- elif play_1.lower()=="s" and play_2.lower()=="p":
+elif play_1.lower()=="s" and play_2.lower()=="p":
    print(f"{player_1} WINS")
- elif play_1.lower()=="p" and play_2.lower()=="s":
+elif play_1.lower()=="p" and play_2.lower()=="s":
    print(f"{player_2} WINS")
- elif play_1.lower()=="r" and play_2.lower()=="p":
+elif play_1.lower()=="r" and play_2.lower()=="p":
    print(f"{player_1} WINS")
- elif play_1.lower()=="p" and play_2.lower()=="r":
+elif play_1.lower()=="p" and play_2.lower()=="r":
    print(f"{player_2} WINS")
- elif play_1.lower()==play_2.lower():
+elif play_1.lower()==play_2.lower():
    print("Its a Tie")
- else:
+else:
    print('\033[31mSOMEBODY ENTERED AN INVALID CHOICE')
- condition=input("Do You Want To Play Again? (yes/no/y/n): ")
- if condition.lower()=="yes" or condition.lower=="y":
-   exit_condition="no"
- else:
-   exit_condition="yes"
+condition=input("Do You Want To Play Again? (yes/no/y/n): ")
+if condition.lower()=="yes" or condition.lower=="y":
+   break
+print("THANK YOU FOR PLAYING")
+
